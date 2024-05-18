34c7906c34effbd8d699251ee1a003ddbc4910b9
Sagar-R-Madankar <166837150+Sagar-R-Madankar@users.noreply.github.com>
2024-05-18 18:52:17 +0530

    task1

---
+++ script.js
@@ -0,0 +1,21 @@
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar');
+navlist = document.querySelector('.nav-list');
+rightnav = document.querySelector('.rightnav');
+
+
+burger.addEventListener('click', ()=>{
+	rightnav.classList.toggle('v-class-resp');
+	navlist.classList.toggle('v-class-resp');
+	navbar.classList.toggle('h-nav-resp');
+});
+document.addEventListener("DOMContentLoaded", function() {
+	const navbar = document.querySelector('.navbar');
+	window.addEventListener('scroll', function() {
+		if (window.scrollY > 500) { 
+			navbar.classList.add('scrolled');
+		} else {
+			navbar.classList.remove('scrolled');
+		}
+	});
+});
